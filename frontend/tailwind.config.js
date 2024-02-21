3ae94d910042cdbaf07bc25cb04272ec94ddd8d3
E M P J De Saram <jayodya7desaram@gmail.com>
2024-02-21 20:43:58 +0530

    UpdateLoginPage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      "3vw": "3vw",
+      "2vw": "2vw",
+      "1.2vw": "1.2vw",
+    },
+    margin: {
+      "15%": "15%",
+    },
   },
   plugins: [],
 };
