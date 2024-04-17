3c03a64c225fce892154bf6c5397f441cf3ab90e
E M P J De Saram <jayodya7desaram@gmail.com>
2024-04-17 09:03:27 +0530

    Display books/ My library

--- frontend/src/Pages/MyLibrary.js
+++ frontend/src/Pages/MyLibrary.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./myLibrary.css";
 import Header from "../Components/Header";
 import { BiSolidEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
-import book1 from "../Pages/images/Harry-Potter-and-the-Philosophers-Stone.jpeg";
 import { useNavigate } from "react-router";
+import axios from "axios";
+import { userContext } from "../App";
 
 export default function MyLibrary() {
   const navigate = useNavigate();
+  const [books, setBooks] = useState([]);
+  const user = useContext(userContext);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5555/book/getBook")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const uploadBook = () => {
     navigate("/Upload-book");
@@ -26,31 +38,39 @@ export default function MyLibrary() {
           </div>
         </div>
         <div className="library-body">
-          <div className="myBook-div">
-            <div className="book-photo-div flex items-center justify-center">
-              <img
-                className="book-photo"
-                src={book1}
-                alt="Harry-Potter-and-the-Philosophers-Stone.jpeg"
-              />
-            </div>
-            <div className="book-details-div">
-              <h2>Harry Potter and the Philosophers Stone</h2>
-              <h3>By J K Rowling</h3>
-              <h4>LKR 850.00</h4>
-              <p>Fiction</p>
-            </div>
-            <div className="book-details-buttons flex items-center justify-around">
-              <button className="library-edit-button flex items-center">
-                Edit
-                <BiSolidEdit />
-              </button>
-              <button className="library-delete-button flex items-center">
-                Delete
-                <MdDelete />
-              </button>
-            </div>
-          </div>
+          {books.map((book) =>
+            user.email === book.email ? (
+              <>
+                <div className="myBook-div">
+                  <div className="book-photo-div flex items-center justify-center">
+                    <img
+                      className="book-photo"
+                      src={`http://localhost:5555/book/Images/${book.file}`}
+                      alt={`${book.file}`}
+                    />
+                  </div>
+                  <div className="book-details-div">
+                    <h2>{book.title}</h2>
+                    <h3>{book.author}</h3>
+                    <h4>LKR {book.price}</h4>
+                    <p>{book.category}</p>
+                  </div>
+                  <div className="book-details-buttons flex items-center justify-around">
+                    <button className="library-edit-button flex items-center">
+                      Edit
+                      <BiSolidEdit />
+                    </button>
+                    <button className="library-delete-button flex items-center">
+                      Delete
+                      <MdDelete />
+                    </button>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <></>
+            )
+          )}
         </div>
       </div>
     </div>
